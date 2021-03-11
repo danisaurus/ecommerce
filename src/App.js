c515577feece01c2a2bd8f1964531969a3c8a1f3
danisaurus <danidirks@gmail.com>
2021-03-11 09:05:15 +0800

    added collection item and styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
@@ -15,7 +11,7 @@ function App() {
       <Switch>
 
             <Route exact path='/' component = {HomePage}/>
-            <Route path='/hats' component = {HatsPage}/>
+            <Route path='/shop' component = {ShopPage}/>
 
       </Switch>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,12 +11,3 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
