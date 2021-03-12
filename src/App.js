4deb49fb48e7ddc0975dcd034cb5a44684e81b69
danisaurus <danidirks@gmail.com>
2021-03-12 18:10:01 +0800

    added sign up form and new user created when you sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -14,7 +14,7 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
 
   }
 
@@ -22,17 +22,31 @@ class App extends React.Component {
 
   componentDidMount() {
     /*onAuthStateChanged is a method on auth library of Firebase takes a function where the parameter is what the user state is*/
-    this.unsubscribeFromAuth = auth.onAuthStateChanged (user => {
-      this.setState({ currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged ( async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
-  }
+        userRef.onSnapshot (snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+        });
+        console.log(this.state);
+      });
 
-  componentWillUnmount(){
+    };
+    this.setState({currentUser: userAuth});
+    
+    });
+  };
+
+  componentWillUnmount() {
     /*This closes the subscription*/
     this.unsubscribeFromAuth();
-  }
+  };
 
   render() {
     return (
