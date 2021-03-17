dfcb66f233907e02ff1048dec8d13d85271b569b
danisaurus <danidirks@gmail.com>
2021-03-17 15:44:44 +0800

    adding in add to cart functionality to root reducer and cart reducer and to collection page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
-import CartActionTypes from '../../redux/cart/cart.types';
 
 import './cart-icon.styles.scss';
 
