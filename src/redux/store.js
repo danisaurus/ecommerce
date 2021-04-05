a3adc5b44d5ebc7e0edd4e433e885fe59b3c1d7e
danisaurus <danidirks@gmail.com>
2021-04-05 11:25:30 +0800

    adding thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
+import thunk from 'redux-thunk';
+
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 /*Below code checks the node environment varliable to see what features or middleware to use or not use*/
 if (process.env.NODE_ENV === 'development'){
